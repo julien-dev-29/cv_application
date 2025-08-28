a817d82ce5a341c1570bb4523074bd218fb19b13
Your Name <you@example.com>
2025-08-28 09:44:31 +0200

    Deploy to netlify

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import { ControlPanel } from "./ControlPanel";
 import { Display } from "./Display/Display";
 
 function App() {
-  const [user, setUser] = useState({
-    firstname: "JULIEN",
-    lastname: "ROLLAND",
-    email: "jurol@jurol.com",
-    tel: "06.28.62.54.85",
-  });
+  const [user, setUser] = useState({});
   const [educationals, setEducationals] = useState([]);
   const [practicals, setPratictals] = useState([]);
   return (
